e8d5e229222abb88a44b354db2e15aa822d94ead
Gene Nakagaki <gene@Genes-MBP.fios-router.home>
2016-11-08 12:25:40 -0500

    setup themoviedb api key

--- app/src/main/java/com/genenakagaki/popularmovies/MainActivity.java
+++ app/src/main/java/com/genenakagaki/popularmovies/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Log.d(LOG_TAG, BuildConfig.THE_MOVIE_DB_API_KEY);
     }
 
     @Override
