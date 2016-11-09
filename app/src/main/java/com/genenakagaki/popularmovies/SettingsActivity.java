445f38b289167c6f0413554138ebdb239f4ef121
Gene Nakagaki <gene@Genes-MBP.fios-router.home>
2016-11-09 14:51:42 -0500

    Added setting activity

---
+++ app/src/main/java/com/genenakagaki/popularmovies/SettingsActivity.java
@@ -0,0 +1,21 @@
+package com.genenakagaki.popularmovies;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by gene on 11/9/16.
+ */
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
